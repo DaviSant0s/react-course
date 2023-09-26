05a4267ae11aefb5a73e79fa61ca2412a98bf0ca
Davi Santos <daviir17@gmail.com>
2023-09-26 12:30:37 -0300

    feat(aula 7): useMemo

--- projectwo/src/App.js
+++ projectwo/src/App.js
@@ -1,30 +1,69 @@
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
-import React, { useCallback, useEffect, useState } from 'react';
 
-const Button = React.memo(function Button({ incrementButton }) {
-  console.log('filho renderizou');
-  return <button onClick={() => incrementButton(100)}>+</button>
-});
+const Post = ( { post } ) => {
+  console.log('Filho renderizou');
+  return(
+    <div key={post.id}>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+
+  );
+}
 
 function App() {
-  const [ counter, setCounter ] = useState(0);
+  console.log('Pai renderizou!');
+  const [ posts, setPosts ] = useState([]);
+  const [ value, setValue ] = useState('');
 
-  const incrementCounter = useCallback(
-    
-    (num) => {
-      setCounter((c) => c + num)
-    
-    }
-    
-    , []);
+  // simular um component did mount
+  useEffect(() => {
+    const buscaDados = async () => {
+      
+      const resposta = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const posts = await resposta.json();
+      setPosts(posts);
+    };
 
-  console.log('pai renderizou');
+    setTimeout(buscaDados, 5000);
+
+
+  }, []);
 
   return (
-    <div className="App">
-      <p>Teste 3</p>
-      <h1>Contador: {counter}</h1>
-      <Button incrementButton={incrementCounter}/>
+    <div className='App'>
+
+      <p>
+        <input
+          type='search'
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+        
+      </p>
+      
+
+      {useMemo(() => {
+        return(
+          
+          posts.length > 0 &&
+            posts.map( post => {
+              return(
+                <Post key={post.id} post={post} />
+              );
+          
+            })
+
+        );
+
+
+      }, [posts])}
+
+     
+
+      {posts.length <= 0 && (<p>'Ainda não existem posts'</p>)}
+
     </div>
   );  
 }
