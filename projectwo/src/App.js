bd6fcfd71d91dac351f392e8565eafe48fe5b34e
Davi Santos <daviir17@gmail.com>
2023-10-18 09:36:45 -0300

    feat(aula 12): useContext + useReducer

--- projectwo/src/App.js
+++ projectwo/src/App.js
@@ -1,40 +1,72 @@
-import { useReducer } from 'react';
+import { createContext, useContext, useReducer } from 'react';
 import './App.css';
+import { useRef } from 'react';
 
-const globalState = {
+// action.js
+export const actions = {
+  CHANGE_TITLE: 'CHANGE_TITLE',
+}
+
+// data.js
+export const globalState = {
   title: 'O título que contexto',
   body: 'O body do contexto',
   counter: 0,
 };
 
-const reducer = (state, action) => {
-  switch(action.type){
-    case 'muda':
-      console.log('chamou muda com', action.payload);
-      return {...state, title: action.payload}
-
-    case 'inverter': {
-      console.log('chamou inverter');
-      const {title} = state;
-      return {...state, title: title.split('').reverse().join('')}
+// reducer.js
+export const reducer = (state, action) => {
+  switch (action.type){
+    case actions.CHANGE_TITLE: {
+      console.log('mudar título');
+      return {...state, title: action.payload};
     }
   }
-
-  console.log('NENHUMA ACTION ENCONTRADA')
-  return { ...state };
+  return {...state};
 }
 
-function App() {
+// AppContext.jsx
+export const Context = createContext();
+export const  AppContext = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, globalState);
-  const {counter, title, body} = state;
+
+  const changeTitle = (payload) => {
+    dispatch({type: actions.CHANGE_TITLE, payload });
+  }
+
+  return(
+    <Context.Provider value={{state, changeTitle}}>
+      {children}
+    </Context.Provider>
+  )
+  
+};
+
+// H1/index.jsx
+export const H1 = () => {
+  const context = useContext(Context);
+  const inputRef = useRef();
+
+  return(
+    <>
+      <h1 onClick={() => context.changeTitle(inputRef.current.value)}>{context.state.title}</h1>
+
+      <input type='text' ref={inputRef}/>
+    </>
+  );
+}
+
+
+
+// App.js
+function App() {
 
   return (
-    <div>
-      <h1>{title} - {counter}</h1>
-      <button onClick={() => dispatch({ type: 'muda', payload: new Date().toLocaleDateString('pt-BR') })}>muda</button>
-      <button onClick={() => dispatch({ type: 'inverter' })}>inverte</button>
-      <button onClick={() => dispatch({ type: 'qualquer' })}>qualquer</button>
-    </div>
+      <AppContext>
+        <div>
+            <H1/>
+        </div>
+      </AppContext>
   );  
 }
 
