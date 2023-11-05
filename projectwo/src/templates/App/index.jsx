236b5d9fbe2ef652c726b79b27331aa20ff81249
Davi Santos <daviir17@gmail.com>
2023-11-04 23:57:36 -0300

    feat(aula 13): organização  com useEffect, useReducer, useContext e useRef - parte 1

---
+++ projectwo/src/templates/App/index.jsx
@@ -0,0 +1,18 @@
+import './styles.css'; 
+import { PostsProvider } from '../../contexts/PostsProvider';
+import { Posts } from '../../components/Posts';
+
+function App() {
+
+  return (
+    <PostsProvider>
+      <div>
+        <Posts/>
+      </div>
+    </PostsProvider>
+  );
+
+
+}
+
+export default App; 
