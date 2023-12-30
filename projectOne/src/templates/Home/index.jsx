06ee8be0481b58508a63f6ef1ec22b09f97d6d49
Davi Santos <daviir17@gmail.com>
2023-12-30 17:58:26 -0300

    feat(hhbb): lmn

--- projectOne/src/templates/Home/index.jsx
+++ projectOne/src/templates/Home/index.jsx
@@ -1,53 +1,16 @@
-import { Children, cloneElement, createContext, useContext, useState } from "react";
+import { useState } from "react";
+import { LazyComponent } from "./lazy-component";
 
-const s = {
-  style: {
-    fontSize: '60px',
-  },
-};
 
-const TurnOnOffContext = createContext();
-
-const TurnOnOff = ({children}) => {
-  const [isOn, setIsOn] = useState(false);
-  const onTurn = () => setIsOn(s => !s);
-
-  return (
-  <TurnOnOffContext.Provider value={{isOn, onTurn}}>
-    {children}
-  </TurnOnOffContext.Provider>
-  )
-}
-
-const TurnedOn = ({children}) => {
-  const { isOn } = useContext(TurnOnOffContext);
-  return isOn ? children : null;
-}
-
-const TurnedOff = ({children}) => {
-  const { isOn } = useContext(TurnOnOffContext);
-  return isOn ? null : children;
-}
-
-const TurnButton = ({...props}) => {
-  const { isOn, onTurn } = useContext(TurnOnOffContext);
-  return <button onClick={onTurn} {...props}>Turn {isOn ? 'OFF' : 'ON'}</button>
-}
-
-const P = ({children}) => <p {...s}>{children}</p>
- 
 export const Home = () => {
+  const [show, setShow] = useState(false);
 
   return (
-    <TurnOnOff>
-
-      <div>
-        <TurnedOn><P>ESTÁ ON</P></TurnedOn>
-        <TurnedOff><P>ESTÁ OFF</P></TurnedOff>
-        <p>oi</p>
-        <TurnButton {...s}/>
-      </div>
-
-    </TurnOnOff >
+    <div>
+      <p>
+        <button onClick={() => setShow(s => !s)}>show {show ? 'ta na tela' : 'não ta na tela'}</button>
+      </p>
+      {show && <LazyComponent/>}
+    </div>
   );
 };
