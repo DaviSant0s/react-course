2171aad7289443b74516b2b5fd6a6655230ed134
Davi Santos <daviir17@gmail.com>
2024-01-11 01:44:20 -0300

    feat(restart): inicio de um novo projeto do curso

--- projectOne/src/templates/Home/index.jsx
+++ projectOne/src/templates/Home/index.jsx
@@ -1,24 +1,7 @@
-import React, { Suspense, useState } from "react";
-
-const loadComponent = () => {
-  console.log('componente carregando...');
-  return import("./lazy-component");
-};
-
-const LazyComponent = React.lazy(loadComponent);
-
 export const Home = () => {
-  const [show, setShow] = useState(false);
-
   return (
     <div>
-      <p>
-        <button onMouseOver={loadComponent} onClick={() => setShow(s => !s)}>show {show ? 'ta na tela' : 'não ta na tela'}</button>
-      </p>
-      <Suspense fallback={<p>Carregando...</p>}>
-        {show && <LazyComponent/>}
-      </Suspense>
-      
+      <h1>Oi</h1>
     </div>
   );
 };
