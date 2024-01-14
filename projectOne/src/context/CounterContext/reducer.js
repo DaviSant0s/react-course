7276b7e4ad473c7a0e9b245dc56646c074beb46f
Davi Santos <daviir17@gmail.com>
2024-01-13 21:08:32 -0300

    feat(aula 29): context API + Hooks (unindo as ideias) - Parte 3

---
+++ projectOne/src/context/CounterContext/reducer.js
@@ -0,0 +1,22 @@
+import { initialState } from './data';
+import * as types from './types';
+
+export const reducer = (state, action) => {
+    switch(action.type){
+        case types.INCREASE:
+            return { ...state, counter: state.counter + 1};
+        case types.DECREASE:
+            return { ...state, counter: state.counter - 1};
+        case types.RESET:
+            return { ...initialState};
+        case types.SET_COUNTER:
+            return { ...state, ...action.payload };
+        case types.ASYNC_INCREASE_START:
+            return { ...state, loading: true };
+        case types.ASYNC_INCREASE_END:
+            return { ...state, loading: false , counter: state.counter + 1};
+        case types.ASYNC_INCREASE_ERROR:
+            return { ...state, loading: false};
+    }
+    return state;
+}
